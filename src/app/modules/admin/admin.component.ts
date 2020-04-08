069e852bb4add5b67a4dadc8df160179d3eb362d
Damian <damianknapik97@gmail.com>
2020-04-08 21:43:35 +0200

    Fix Heroku deployment errors

--- src/app/modules/admin/admin.component.ts
+++ src/app/modules/admin/admin.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
-  templateUrl: '/admin.component.html' ,
-  styleUrls: ['/admin.component.sass']
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.sass']
 })
 export class AdminComponent implements OnInit {
 
