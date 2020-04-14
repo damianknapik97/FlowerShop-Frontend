b57a4ab304a8ca9e6a435acfc9c24b9bd7f238f5
Damian <damianknapik97@gmail.com>
2020-04-14 23:40:52 +0200

    Improve main page and employee page layout in mobile devices context

--- src/app/modules/employee/order-administration/order-administration.component.ts
+++ src/app/modules/employee/order-administration/order-administration.component.ts
@@ -1,41 +1,48 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { MatSnackBar } from '@angular/material';
+import { OrderAdministrationService } from 'src/app/core/services/administration/order-administration.service';
 import { OrderDTO } from 'src/app/core/dto/order';
 import { RestPage } from 'src/app/core/dto';
-import { OrderAdministrationService } from 'src/app/core/services/administration/order-administration.service';
-import { error } from 'protractor';
-import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-order-administration',
   templateUrl: './order-administration.component.html',
-  styleUrls: ['./order-administration.component.sass']
+  styleUrls: ['./order-administration.component.sass'],
 })
 export class OrderAdministrationComponent implements OnInit {
   @Input() public page = 0;
   @Input() public pageSize = 20;
   @Input() public collectionSize = 0;
-  @Input() public sortingProperties: string[] = [
-    'NONE'
-  ];
+  @Input() public sortingProperties: string[] = ['NONE'];
   public dataLoaded = false;
   public content: OrderDTO[];
 
-  constructor(private service: OrderAdministrationService, private snackBar: MatSnackBar) { }
+  constructor(
+    private service: OrderAdministrationService,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit() {
     this.retrieveSortingProperties();
-    this.retrieveOrdersPage(this.page, this.pageSize, this.sortingProperties[0]);
-  }
-
-  public retrieveSortingProperties(): void {
-    this.service.retriveSortingProperties().subscribe(
-      (res: string[]) => {
-        this.sortingProperties = res;
-      }
+    this.retrieveOrdersPage(
+      this.page,
+      this.pageSize,
+      this.sortingProperties[0]
     );
   }
 
-  public retrieveOrdersPage(page: number, pageSize: number, sortingProperty: string): void {
+  public retrieveSortingProperties(): void {
+    this.service.retriveSortingProperties().subscribe((res: string[]) => {
+      this.sortingProperties = res;
+    });
+  }
+
+  public retrieveOrdersPage(
+    page: number,
+    pageSize: number,
+    sortingProperty: string
+  ): void {
     this.service.retrieveOrdersPage(page, pageSize, sortingProperty).subscribe(
       (result: RestPage<OrderDTO>) => {
         this.page = result.number;
@@ -45,10 +52,11 @@ export class OrderAdministrationComponent implements OnInit {
         this.dataLoaded = true;
       },
       (err: any) => {
-        this.snackBar.open('Couldn\t retrieve orders page', 'Error', {duration: 3000});
+        this.snackBar.open('Couldn\t retrieve orders page', 'Error', {
+          duration: 3000,
+        });
         console.log(err);
       }
     );
   }
-
 }
