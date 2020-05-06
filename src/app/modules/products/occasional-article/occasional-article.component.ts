ad547285806f96e8c72a0bf6c198ff974211d052
Damian <damianknapik97@gmail.com>
2020-05-06 23:04:42 +0200

    Add product image spinner

--- src/app/modules/products/occasional-article/occasional-article.component.ts
+++ src/app/modules/products/occasional-article/occasional-article.component.ts
@@ -20,21 +20,46 @@ export class OccasionalArticleComponent implements OnInit {
   public viewModel: OccasionalArticleDTO[][];
   public elemntsInRow = 3;
   @Input() page = 1;
-  @Input() pageSize;
+  @Input() pageSize = 12;
   @Input() collectionSize;
+  private imageLoaded: boolean[] = []; // Used to store information about loaded in viewModel
 
   constructor(
     private service: OccasionalArticleService,
     private arrayUtils: ArrayUtilities,
     private snackBar: MatSnackBar,
     public authenticationGuard: AuthenticationGuard
-  ) {}
+  ) {
+    this.resetLodadedImages(this.pageSize);
+  }
 
   ngOnInit() {
     this.getOccasionalArticlesPage(this.page);
   }
 
+  /* Returns image status for provided index */
+  public isImageLoaded(imageIndex: number): boolean {
+    return this.imageLoaded[imageIndex % this.pageSize];
+  }
+
+  /* Initializes array with false boolean values based on number of provided images from function argument. */
+  private resetLodadedImages(totalImages: number): void {
+    for (let i = 0; i < totalImages; i++) {
+      this.imageLoaded[i] = false;
+    }
+  }
+
+  /* Sets status of loading image inside booleans array. */
+  public onImageLoad(imageIndex: number): void {
+    /* Timeout is set in order to avoid flickering during fast page changing. */
+    setTimeout(
+      () => (this.imageLoaded[imageIndex % this.pageSize] = true),
+      150
+    );
+  }
+
   public onChangePage(pageNumber: number) {
+    this.resetLodadedImages(this.pageSize);
     this.getOccasionalArticlesPage(pageNumber);
   }
 
